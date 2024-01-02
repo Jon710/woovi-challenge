db05379fa7056e11c682fdc1432244eb4db53122
João Luis Moraes <jonmoraesNL@gmail.com>
2024-01-01 23:37:26 -0500

    feat: PatientSignInMutation

--- server/src/modules/patient/mutations/PatientSignInMutation.ts
+++ server/src/modules/patient/mutations/PatientSignInMutation.ts
@@ -1,13 +1,10 @@
 import { GraphQLNonNull, GraphQLString } from "graphql";
 import { mutationWithClientMutationId } from "graphql-relay";
-import { successField } from "@entria/graphql-mongo-helpers";
-import { UserType } from "../UserType";
-import { UserModel } from "../UserModel";
-import * as PatientLoader from "../PatientLoader";
+
+import { PatientType } from "../PatientType";
+import { PatientModel } from "../PatientModel";
+import { PatientLoader } from "../PatientLoader";
 import { setAuthCookie } from "../../../auth";
-import { fieldError } from "../../../utils/fieldError";
-import { GraphQLContext } from "../../../graphql/context";
-import { FieldErrorField } from "../../field-error/FieldErrorField";
 
 interface PatientSignInMutationArgs {
   email: string;
@@ -17,47 +14,29 @@ interface PatientSignInMutationArgs {
 const PatientSignInMutation = mutationWithClientMutationId({
   name: "PatientSignInMutation",
   inputFields: {
-    email: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    password: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
+    email: { type: new GraphQLNonNull(GraphQLString) },
+    password: { type: new GraphQLNonNull(GraphQLString) },
   },
-  mutateAndGetPayload: async (
-    args: PatientSignInMutationArgs,
-    { ctx }: GraphQLContext
-  ) => {
-    const { email, password } = {
-      password: args.password.trim(),
-      email: args.email.trim().toLowerCase(),
-    };
+  mutateAndGetPayload: async (args: PatientSignInMutationArgs, { ctx }) => {
+    const { email, password } = args;
 
-    const user = await UserModel.findOne({ email });
+    const user = await PatientModel.findOne({ email });
 
-    if (!user) return fieldError("email", "User not found");
+    if (!user) throw new Error("Patient not found!");
 
     const isPasswordCorrect = user.authenticate(password);
 
-    if (!isPasswordCorrect) {
-      // Could improve security by logging an attempt of login on redis and setting a max amount
-      return fieldError("password", "Wrong password");
-    }
+    if (!isPasswordCorrect) throw new Error("Wrong password! Try again.");
 
     setAuthCookie(ctx, user);
 
-    return {
-      id: user._id,
-      success: "Sign In Successful",
-    };
+    return { id: user._id, success: "User is signed in!" };
   },
   outputFields: {
     me: {
-      type: UserType,
+      type: PatientType,
       resolve: ({ id }, _, context) => PatientLoader.load(context, id),
     },
-    ...FieldErrorField,
-    ...successField,
   },
 });
 
